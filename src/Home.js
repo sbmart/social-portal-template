ca61225a61e3d613595db953357a6786ff5eeb16
sbmart <sbmart73@gmail.com>
2021-06-04 19:43:42 +0300

    Refactor monolith layout into small components

---
+++ src/Home.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Grid,
+    Image
+} from 'semantic-ui-react'
+
+function Home() {
+    const [data, setData] = useState([]);
+    const getData = () => {
+        fetch('localAPI.json'
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson)
+            });
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+    return (
+        <Grid>
+            <Grid.Column width={4}>
+                < Image src='./image.png' rounded />
+            </Grid.Column>
+            <Grid.Column width={12}>
+                {
+                    data && data.length > 0 && data.map((item) => <p key={item._id}> {item.about}</p>)
+                }
+            </Grid.Column>
+        </Grid>
+    )
+}
+export default Home
