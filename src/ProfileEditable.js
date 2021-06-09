a7ff1327604fb130c8374216948f933e592de9b8
sbmart <sbmart73@gmail.com>
2021-06-09 12:41:20 +0300

    profile layout finished

--- src/ProfileEditable.js
+++ src/ProfileEditable.js
@@ -1,9 +1,10 @@
 import React
     //, { useState, useEffect }
     from 'react';
-import { Button, Form, TextArea, Header, Image, Message, Segment, Select, Grid, Container } from 'semantic-ui-react';
+import { Button, Divider, Form, TextArea, Header, Image, Input, Message, Segment, Select, Grid, Container } from 'semantic-ui-react';
 import './input.css'
 const aimOptions = [
+    // { key: 'none', value: 'none', text: 'ушли' },
     { key: 'sport', value: 'sport', text: 'Занятия спортом' },
     { key: 'music', value: 'music', text: 'Занятия музыкой' },
     { key: 'paint', value: 'paint', text: 'Занятия живописью' },
@@ -33,34 +34,59 @@ function ProfileEditable() {
     //     getData()
     // }, [])
     return (
-        <>
-            <Grid textAlign='left' style={{ height: '100vh' }} verticalAlign='top'>
-                <Grid.Column style={{ maxWidth: 650 }}>
-                    <Header as='h2' color='blue' textAlign='center'>
-                        Log-in to your account
-                    </Header>
-                    <Form size='big'>
-                        <Segment stacked>
-                            <Form.Input icon='user' iconPosition='left' placeholder='E-mail address' />
-                            <Form.Input
-                                fluid
-                                icon='lock'
-                                iconPosition='left'
-                                placeholder='Password'
-                                type='password'
-                            />
+        <Form>
+            <Form.Group widths={2}>
+                <Form.Input fluid label='E-mail' placeholder='Sbmart@ya.ru' readOnly />
+            </Form.Group>
+            {/* <Form.Group unstackable widths={2}>
+                <Form.Input label='First name' placeholder='First name' />
+            </Form.Group> */}
+            <Form.Group widths={2}>
+                <Form.Input label='Имя' placeholder='Имя' />
+            </Form.Group>
 
-                            <Button color='blue' fluid size='large'>
-                                Login
-          </Button>
-                        </Segment>
-                    </Form>
-                    <Message>
-                        New to us? <a href='#'>Sign Up</a>
-                    </Message>
-                </Grid.Column>
-            </Grid>
-        </>
+            {/* ДАТА РОЖДЕНИЯ */}
+            <Form.Group unstackable widths={5}>
+                <Form.Select fluid label='День рождения' options={aimOptions} placeholder='День' />
+                <Form.Select fluid label='месяц' options={aimOptions} placeholder='Месяц' />
+                <Form.Select fluid label='год' options={aimOptions} placeholder='Год' />
+            </Form.Group>
+
+            {/* ЦЕЛЬ ЗНАКОМСТВА */}
+            <Form.Group unstackable widths={3}>
+                <Form.Select fluid label='Цель знакомства' options={aimOptions} placeholder='Выберите' />
+            </Form.Group>
+
+
+            {/* ИЩУ ПАРТНЕРА ВОЗРАСТОМ */}
+            <h4>Ищу партнера возрастом</h4>
+            <Form.Group>
+                <Form.Field inline>
+                    <label>От</label>
+                    <Select options={aimOptions} placeholder='-' />
+                </Form.Field>
+
+                <Form.Field inline>
+                    <label>До</label>
+                    <Select options={aimOptions} placeholder='--' />
+                </Form.Field>
+
+            </Form.Group>
+
+            {/* АДРЕС */}
+            <Form.Group widths={2}>
+                <Form.Input label='Адрес' placeholder='Адрес' />
+            </Form.Group>
+
+            {/* О СЕБЕ */}
+            <h4>О себе</h4>
+            <Form style={{ width: 500 }}>
+                <TextArea placeholder='Расскажите о себе' />
+            </Form>
+            <Divider hidden />
+            <Form.Checkbox label='Согласен с пользовательским соглашением и политикой обработки персональных данных' />
+            <Button positive type='submit'>Сохранить</Button>
+        </Form>
     )
 }
 
