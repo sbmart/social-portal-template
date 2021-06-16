99223bf02f9e77d71820d26b1e38c06424c91ec9
sbmart <sbmart73@gmail.com>
2021-06-16 18:16:00 +0300

    step 3 done

--- src/RightPanel.js
+++ src/RightPanel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { selectAllPosts, fetchPosts } from './features/posts/postSlice'
 import { Segment } from 'semantic-ui-react';
@@ -7,28 +7,8 @@ function RightPanel() {
     const dispatch = useDispatch()
     const posts = useSelector(selectAllPosts)
     const postStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
 
-    const [data, setData] = useState([]);
-    const getData = () => {
-        fetch('localAPI.json'
-            , {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }
-        )
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (myJson) {
-                console.log(myJson);
-                setData(myJson)
-            });
-    }
-    // useEffect(() => {
-    //     getData()
-    // }, [])
     useEffect(() => {
         if (postStatus === 'idle') {
             setTimeout(() => {
@@ -36,12 +16,20 @@ function RightPanel() {
             }, 1000);
         }
     }, [postStatus, dispatch])
+
+    let content
+
+    if (postStatus === 'idle' || postStatus === 'loading') {
+        content = <div className="loader">Loading...</div>
+    } else if (postStatus === 'succeeded') {
+        console.log(posts.posts)
+        content = posts.posts.map((item) => <Segment key={item._id}> {item.about}</Segment>)
+    } else if (postStatus === 'failed') {
+        content = error
+    }
     return (
         <>
-
-            {
-                data && data.length > 0 && data.map((item) => <Segment key={item._id}> {item.about}</Segment>)
-            }
+            {content}
         </>
     )
 }
