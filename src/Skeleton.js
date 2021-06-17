69422ded1691058bbfb74797a9df6948cc642cb5
sbmart <sbmart73@gmail.com>
2021-06-17 11:35:39 +0300

    render Skeletons with map

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -2,46 +2,27 @@ import React from 'react'
 import { Placeholder, Segment } from 'semantic-ui-react'
 
 function Skeleton() {
+    const n = 7 // количество скелетов "в шкафу))"
     return (
         <>
-            <Placeholder fluid>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                </Placeholder.Paragraph>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                </Placeholder.Paragraph>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                </Placeholder.Paragraph>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                </Placeholder.Paragraph>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                </Placeholder.Paragraph>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                    <Placeholder.Line />
-                </Placeholder.Paragraph>
-            </Placeholder>
+            {/* создаем массив из n undefined элементов и мапим его по ключу i */}
+            {[...Array(n)].map((i) =>
+                <Segment key={i} raised>
+                    <Placeholder>
+                        <Placeholder.Header image>
+                            <Placeholder.Line />
+                            <Placeholder.Line />
+                        </Placeholder.Header>
+                        <Placeholder.Paragraph>
+                            <Placeholder.Line length='medium' />
+                            <Placeholder.Line length='short' />
+                            <Placeholder.Line length='full' />
+                            {/* <Placeholder.Line length='very long' /> */}
+                        </Placeholder.Paragraph>
+                    </Placeholder>
+                </Segment>
+            )}
+
         </>
     )
 }
