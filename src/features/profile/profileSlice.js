cb813ab1517c15f84c93cb4ed490eb3a500f0133
sbmart <sbmart73@gmail.com>
2021-06-21 12:00:36 +0300

    #29 (#38)
    * profile slice added
    
    * step 1
    
    * step 2
    
    * #29 done with some issues
    
    * fixed typo

--- src/features/profile/profileSlice.js
+++ src/features/profile/profileSlice.js
@@ -3,19 +3,28 @@ import { createSlice } from '@reduxjs/toolkit'
 export const profileSlice = createSlice({
     name: 'profile',
     initialState: {
-        firstName: "John",
+        userCard: {
+            email: "",
+            firstName: "",
+            birthDateD: "",
+            birthDateM: "",
+            birthDateY: "",
+            aim: "",
+            ageFrom: 0,
+            ageTo: 0,
+            adress: "",
+            about: "",
+        },
     },
     reducers: {
-        increment: (state) => {
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
+        editProfile: (state, action) => {
+            state.profile = action.payload
+        }
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement } = profileSlice.actions
+export const { editProfile } = profileSlice.actions
+export const selectProfile = state => state.profile
 
 export default profileSlice.reducer
