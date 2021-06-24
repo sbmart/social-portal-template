db18c88e4eff094e2894f27f7cb781c855508a76
sbmart <sbmart73@gmail.com>
2021-06-24 11:54:35 +0300

    norm-url reupdated to 4.5.1

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -7,7 +7,9 @@ function Skeleton() {
         <>
             {/* создаем массив из n undefined элементов и мапим его по ключу i */}
             {[...Array(n)].map((i) =>
-                <Segment key={i} raised>
+                <Segment
+                    // key={i} 
+                    raised>
                     <Placeholder>
                         <Placeholder.Header image>
                             <Placeholder.Line />
